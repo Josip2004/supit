a99bb068270c3b2f2f196f70d8b66d7987cb74ba
Josip2004 <josip77gj@gmail.com>
2024-12-26 19:40:02 +0100

    login and register added forms

---
+++ scripts/login.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('form');
+const userName = document.getElementById('username');
+const password = document.getElementById('password');
+
+// form.addEventListener('submit', (event) => {
+ 
+//   let errors = []
+
+  
+// });
+
